382a4cfdbcfd8d1e64728cb5891f3574939785c0
gago <dragan@kokadigital.com>
2020-12-07 21:18:32 +0100

    change to https protocol for api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 module.exports = {
     API_KEY: '8bd6d76ed85b4c24801113243202911',
-    API_URL: 'http://api.weatherapi.com/v1/forecast.json',
+    API_URL: 'https://api.weatherapi.com/v1/forecast.json',
   };
